2fb297fbee9c5857c716b3d5bc0fd355367457eb
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-03-16 14:44:02 -0400

    Task 2: Adding Risk Items Dynamically

---
+++ cc_15.js
@@ -0,0 +1,8 @@
+// Task 2: Adding Risk Items Dynamically
+function addRiskItem(riskName, riskLevel, department) {
+    const riskCard = document.createElement("div");
+    riskCard.classList.add("riskCard");
+
+    riskCard.innerHTML = `<strong>Risk Name:</strong> ${riskName}<br>
+                          <strong>Risk Level:</strong> ${riskLevel}<br>
+                          <strong>Department:</strong> ${department}<br>`;
